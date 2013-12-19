2dbc086c78a9d841e98cab0704845236dea21118
Andrew Warren <anwarren@vbi.vt.edu>
2013-12-19 00:06:02 -0500

    adding file for generating tables from patric

---
+++ get_figinfo.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on Wed Dec 18 22:04:38 2013
+
+@author: anwarren
+"""
+
+import requests, json, sys
+
+
+
+
+taxURL="http://macleod.vbi.vt.edu:8080/solr/genomesummary/select/?"
+taxQuery="q=rast_cds:[1+TO+*]+AND+taxon_lineage_ids:"
+taxFormat="&indent=on&wt=json&fl=genome_name,genome_info_id,ncbi_tax_id,taxon_lineage_ids"
+
+feature_url='http://macleod.vbi.vt.edu:8080/solr/figfam/select/?'
+
+##get SOLR query results
+def get_solr_result(query_url):
+    results=[]
+    start=0
+    rows=10000
+    currentQuery=query_url+"&start="+str(start)+"&rows=1"
+    response=requests.get(currentQuery)
+    if(not response.ok):
+       print "PATRIC solr API not responding"
+       return results
+    result_summary=json.loads(response.content)
+    try:
+        numFound=result_summary['response']['numFound']
+    except (KeyError):
+        print "Error in getting results returned from PATRIC"
+        return results
+    while start < numFound:
+        if (numFound-start)< rows:
+            limit=numFound-start
+        else:
+            limit=rows
+        try:
+            currentQuery=query_url+"&start="+str(start)+"&rows="+str(rows)
+            response=requests.get(currentQuery)
+            assert response.ok
+            json_result=json.loads(response.content)
+            cur_rows=json_result['response']['docs']
+        except (AssertionError, KeyError,IndexError):
+            print "PATRIC API stopped responding"
+            return results
+        results=results+cur_rows    
+        start=start+rows
+    return results
+				
+
+def patric_figinfo_from_solr(tax_id, target_tax_path, target_figfam_path):
+    format_string="indent=on&wt=json"
+    
+    currentQuery=taxURL+taxQuery+str(tax_id)+taxFormat
+    
+
+    #get replicon results
+    taxonomy_results=get_solr_result(currentQuery)
+    out_handle=open(target_tax_path,'w')
+    #create header
+    if len(taxonomy_results) and 'genome_name' in taxonomy_results[0]:
+        print "Creating tax table from SOLR for "+currentQuery
+
+    gidQuery="gid:"
+    gids=set()
+    for r in taxonomy_results:
+        try:
+            out_handle.write("\t".join([r['genome_name'],r['genome_info_id'],r['ncbi_tax_id'],','.join(r['taxon_lineage_ids'])])+"\n")
+        except:
+            print "couldn't write line "+str(r)
+        if 'genome_info_id' in r:
+            gids.add(str(r["genome_info_id"]))
+    out_handle.close()
+    out_handle=open(target_figfam_path,'w')
+    
+    gidQuery=gidQuery+"+OR+".join(gids)
+    currentQuery=feature_url+"q=("+gidQuery+")"+"&"+"sort=sequence_info_id+asc,start_max+asc"+"&"+format_string
+    feature_results=get_solr_result(currentQuery)
+
+    for f in feature_results:
+        try:
+            out_handle.write("\t".join([f["figfam_id"],f["ncbi_tax_id"],f["sequence_info_id"],f["start_max"]])+"\n")
+        except:
+            print "couldn't write line "+str(f)
+            
+
+def main(init_args):
+    patric_figinfo_from_solr(234, './new_brucella_taxon.txt', './new_brucella_figfam.txt')
+
+if __name__ == "__main__":
+	main(sys.argv[1:])												
